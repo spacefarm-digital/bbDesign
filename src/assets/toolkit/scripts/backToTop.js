4f0a5a56e442f3b957d14746ab6ab816ce09fb1d
Darin Dimitroff <d@thecrazyones.agency>
2017-06-23 13:09:53 +0300

    Check if component is present

--- src/assets/toolkit/scripts/backToTop.js
+++ src/assets/toolkit/scripts/backToTop.js
@@ -1,10 +1,12 @@
 const backToTop = document.querySelector('#js-backToTop');
 
-$(window).scroll(function() {
-   if($(window).scrollTop() > $(window).height()) {
-       backToTop.classList.remove('down-self');
-   }
-   else {
-    backToTop.classList.add('down-self');
-   }
-});
+if (backToTop) {
+    $(window).scroll(function() {
+    if($(window).scrollTop() > $(window).height()) {
+        backToTop.classList.remove('down-self');
+    }
+    else {
+        backToTop.classList.add('down-self');
+    }
+    });
+}
